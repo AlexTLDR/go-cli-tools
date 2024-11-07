9f247cb1a9a1680a056e5fe2f5782fc97b0551d7
AlexTLDR <alex.badragan@protonmail.com>
2024-11-07 12:37:45 +0100

    Capturing Input from STDIN

--- todo/README.md
+++ todo/README.md
@@ -9,4 +9,11 @@ go run main.go -complete 1
 ## Using env variables
 ```
 export TODO_FILENAME=new-todo.json
+unset TODO_FILENAME
+```
+
+## Capturing Input from STDIN
+```
+go run main.go -add "item from Args"
+echo "this one is from STDIN via the echo command" | go run main.go -add
 ```
--- todo/cmd/todo/main.go
+++ todo/cmd/todo/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"github.com/AlexTLDR/go-cli-tools/todo"
+	"io"
 	"os"
+	"strings"
 )
 
 var todoFilename = ".todo.json"
@@ -16,7 +19,7 @@ func main() {
 		fmt.Fprintln(flag.CommandLine.Output(), "Usage information:")
 		flag.PrintDefaults()
 	}
-	task := flag.String("task", "", "Task to be included in the ToDo list")
+	add := flag.Bool("add", false, "Add task to the ToDo list")
 	list := flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
 	flag.Parse()
@@ -43,8 +46,13 @@ func main() {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-	case *task != "":
-		l.Add(*task)
+	case *add:
+		t, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		l.Add(t)
 		if err := l.Save(todoFilename); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
@@ -53,4 +61,19 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Invalid option")
 		os.Exit(1)
 	}
+}
+
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+	if len(scanner.Text()) == 0 {
+		return "", fmt.Errorf("task cannot be blank")
+	}
+	return scanner.Text(), nil
 }
